bfa2664a9e1705acf3fd2e7a3e90a5fa51567515
sajrashid <sajid.rashid@live.co.uk>
2021-07-01 04:45:22 +0100

    updating custom col example

--- src/examples/customcolumns/index.js
+++ src/examples/customcolumns/index.js
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Table from "react-dj-table";
-import cars from "./data.json";
+import "semantic-ui-css/semantic.min.css";
+import data from "/data.json";
 
 function App() {
   const options = {
-    tableCss: "table-fixed cursor-pointer w-full",
+    tableCss: "ui fixed green table",
+    iconCols: [{ email: '<i class="envelope icon"></i>email' }],
+    customCols: [
+      {
+        Avatar:
+          '<div style="min-height:5em"><img  decoding="async" src=${Avatar}></img></div'
+      }
+    ]
   };
   return (
     <div className="App">
-      <Table json={cars} options={options} />
+      <Table json={data} options={options} />
       <h1>Well, yeah.</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
